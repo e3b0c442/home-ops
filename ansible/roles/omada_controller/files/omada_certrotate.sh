de55ebd3445da097f533e0b4f91d559a521c23c1
Nick Meyer <nick@e3b0c442.dev>
2023-05-14 11:33:16 -0500

    remove omada controller plays

--- ansible/roles/omada_controller/files/omada_certrotate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Copy the new cert files to the known location
-cp /etc/letsencrypt/live/ourparadisefalls.com/fullchain.pem /opt/tplink/EAPController/cert/tls.crt
-cp /etc/letsencrypt/live/ourparadisefalls.com/privkey.pem /opt/tplink/EAPController/cert/tls.key
-
-# Restart the Omada Controller
-systemctl restart omada-controller
